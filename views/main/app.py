b1878a5e11920f8afc411bc4ed56e7d29b2ebc2c
ppparamos <pablo.perez.paramos@gmail.com>
2023-05-19 10:26:40 +0200

    Otro commit inicial

--- views/main/app.py
+++ views/main/app.py
@@ -2,9 +2,9 @@ import json
 import flask
 import flask_login
 import sirope
-from model.usuario import Usuario
+# from model.usuario import Usuario
 
-
+"""
 def create_app():
     lmanager = flask_login.login_manager.LoginManager()
     fapp = flask.Flask(__name__)
@@ -30,4 +30,26 @@ def unauthorized_handler():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run() """
+
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def get_index():
+    return flask.send_from_directory(app.static_folder, "index.html")
+
+
+@app.route("/hi", methods=["POST"])
+def post_answer():
+    nombre = flask.request.form["edName"]
+    return ("<html><body>"
+    + f"<b>Hola, {nombre}</b><form>"
+    + "<INPUT Type='button'"
+    + "VALUE='<<' onClick="
+    + "'history.go(-1);"
+    + "return true;'/>"
+    + "</form></body></html>")
