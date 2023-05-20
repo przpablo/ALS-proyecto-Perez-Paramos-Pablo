a3c64d6192e96fe6e50f703db8d823e83877eaa2
ppparamos <pablo.perez.paramos@gmail.com>
2023-05-20 19:14:13 +0200

    Gestion sesion

--- views/main/app.py
+++ views/main/app.py
@@ -1,12 +1,47 @@
+import os
+import flask
+import sirope
+import flask_login
 from flask import Flask, render_template, redirect, url_for, request
+from flask_login import login_manager
 from views.auth.auth import auth_blueprint
 from views.search.home import home_blueprint
+from model.usuario import Usuario
 
-app = Flask(__name__)
 
-# Registrar el blueprint con la ruta correcta
-app.register_blueprint(auth_blueprint, url_prefix='/auth')
-app.register_blueprint(home_blueprint, url_prefix='/home')
+def create_app():
+    lmanager = login_manager.LoginManager()
+    aplicacion = flask.Flask(__name__, instance_relative_config=True)
+    syrp = sirope.Sirope()
+
+    # config_path = os.path.join(aplicacion.root_path, '..', '..', 'instance', 'config.json')
+    # aplicacion.config.from_json(config_path)
+    lmanager.init_app(aplicacion)
+    aplicacion.register_blueprint(auth_blueprint, url_prefix='/auth')
+    aplicacion.register_blueprint(home_blueprint, url_prefix='/home')
+
+    return aplicacion, lmanager, syrp
+
+
+app, lm, srp = create_app()
+
+
+@lm.user_loader
+def user_loader(email):
+    return Usuario.find(srp, email)
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    flask.flash("User logged out")
+    return flask.redirect("/")
+
+
+@lm.unauthorized_handler
+def unauthorized_handler():
+    flask.flash("Unauthorized")
+    return flask.redirect("/")
 
 
 # Ruta principal
