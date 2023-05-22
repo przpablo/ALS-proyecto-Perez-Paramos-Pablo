641682cbfa245d7b4c84846749cc9cb92b5c46a0
ppparamos <pablo.perez.paramos@gmail.com>
2023-05-22 18:26:11 +0200

    Login, Registro, Publicar, Add y Modify Coche

--- views/car/car.py
+++ views/car/car.py
@@ -14,35 +14,63 @@ def get_blprint():
 
 
 car_blueprint, srp = get_blprint()
+usuario = Usuario.current_user()
+
+
+def comprobaciones():
+    marca = request.form['marca']
+    modelo = request.form['modelo']
+    color = request.form['color']
+    anno = request.form['anno']
+
+    if not marca or not marca.isalpha():
+        flask.flash("El formato de la marca no es válido")
+        return False
+    if not modelo:
+        flask.flash("El formato del modelo no es válido")
+        return False
+    if not color or not color.isalpha():
+        flask.flash("El formato del color no es válido")
+        return False
+    if not anno or not anno.isdigit() or int(anno) < 1900 or int(anno) > 2023:
+        flask.flash("El formato del año no es válido")
+        return False
+    else:
+        car = Coche(marca, modelo, color, anno)
+        usuario.coche = car
+        srp.save(car)
+        srp.save(usuario)
+        return True
 
 
 @car_blueprint.route('/car', methods=['GET', 'POST'])
 @login_required
 def add_car():
-    if request.method == 'POST':
-        marca = request.form['marca']
-        modelo = request.form['modelo']
-        color = request.form['color']
-        anno = request.form['anno']
+    if usuario.coche is not None:
+        flask.flash("El usuario ya tiene un coche. No puedes añadir otro, pero sí modificarlo.")
+        return flask.redirect(url_for('home.home_route'))
 
-        if not marca or not marca.isalpha():
-            flask.flash("El formato de la marca no es válido")
-            return flask.redirect("/car/addcar")
-        if not modelo:
-            flask.flash("El formato del modelo no es válido")
-            return flask.redirect("/car/addcar")
-        if not color or not color.isalpha():
-            flask.flash("El formato del color no es válido")
-            return flask.redirect("/car/addcar")
-        if not anno or not anno.isdigit() or int(anno) < 1900 or int(anno) > 2023:
-            flask.flash("El formato del año no es válido")
+    if request.method == 'POST':
+        if not comprobaciones():
             return flask.redirect("/car/addcar")
         else:
-            car = Coche(marca, modelo, color, anno)
-            usuario = Usuario.current_user()
-            usuario.coche = car
-            srp.save(car)
-            srp.save(usuario)
             return flask.redirect(url_for('home.home_route'))
 
     return render_template('addcar.html')
+
+
+@car_blueprint.route('/modcar', methods=['GET', 'POST'])
+@login_required
+def mod_car():
+    if usuario.coche is None:
+        flask.flash("El usuario no tiene un coche, no puedes modificarlo")
+        return flask.redirect(url_for('home.home_route'))
+
+    if request.method == 'POST':
+        if not comprobaciones():
+            return flask.redirect(url_for('car.mod_car'))
+        else:
+            return flask.redirect(url_for('home.home_route'))
+
+    return render_template('modcar.html', coche=usuario.coche)
+
